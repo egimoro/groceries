f753dec7ea82a30bafb2231acbce36b488af206d
egimoro <gimoremma@gmail.com>
2019-06-11 15:34:13 +0200

    test execution

--- grocery_store/groceries/tests.py
+++ grocery_store/groceries/tests.py
@@ -1,4 +1,18 @@
 from django.test import TestCase
+import datetime
+from .models import Groceries, Grocery_store
+from django.utils import timezone
 
-# Create your tests here.
- 
+
+class Grocery_storeModelTests(TestCase):
+    def recent_establishment_in_future_date_test(self):
+        time = timezone.now()+datetime.timedelta(days=30)
+        future_date = Grocery_store(date_established=time)
+        self.assertIs(future_date.was_established_recently(), False) 
+
+
+class GroceriesModelTests(TestCase):
+    def recent_ordered_in_future_date_test(self):
+        time = timezone.now()+datetime.timedelta(days=30)
+        future_date = Groceries(date_ordered=time)
+        self.assertIs(future_date.was_ordered_recently(), False) 
